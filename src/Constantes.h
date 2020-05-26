4564b426ae7504997c0d337e34f2ce386d1e5337
nicolaslongo <nicolaslongo@github.com>
2020-05-25 21:24:52 -0300

    bueno. esto funciona

--- src/Constantes.h
+++ src/Constantes.h
@@ -1,6 +1,8 @@
 #ifndef CONSTANTES_H_
 #define CONSTANTES_H_
 
+#include <string.h>
+
 const int PARENT_PROCESS = 0;
 const int CHILD_PROCESS = 1;
 
@@ -8,9 +10,9 @@ const int PEDIDO_NULO = 0;
 const int PEDIDO_PAN_FLAG = 1;
 const int PEDIDO_PIZZA_FLAG = 2;
 
-const char* const PEDIDO_PAN = "Pedido de tipo P1\n";
-const char* const PEDIDO_PIZZA = "Pedido de tipo P2\n";
-const int LARGO_PEDIDO = 18;
+const char* const PEDIDO_PAN = "Pedido de Panes\n";
+const char* const PEDIDO_PIZZA = "Pedido de Pizza\n";
+const int LARGO_PEDIDO = strlen(PEDIDO_PAN);
 
 const char* const PEDIDO_MM = "Solicito una ración de Masa Madre al compañero Especialista\n";
 
