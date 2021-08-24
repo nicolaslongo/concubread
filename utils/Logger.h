183978825c6690ceaaa19af65b2bfc6d9896a5b1
Nicolás Longo <longo.gnr@hotmail.com>
2021-08-24 17:00:48 -0300

    Update Logger.h

--- utils/Logger.h
+++ utils/Logger.h
@@ -12,18 +12,19 @@ using namespace std;
 class Logger {
 
     private:
-		struct flock fl;
-		int fd;
-		std::string nombre;
+	struct flock fl;
+	int fd;
+	std::string nombre;
 
     protected:
 
     public:
         Logger();
         ~Logger();
-		int lockLogger();
-		int unlockLogger();
-		ssize_t writeToLogFile(const char* buffer, const ssize_t buffsize) const;
+	
+	int lockLogger();
+	int unlockLogger();
+	ssize_t writeToLogFile(const char* buffer, const ssize_t buffsize) const;
         void closeFileInThisScope();
 };
 
